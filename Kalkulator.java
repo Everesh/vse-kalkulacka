c143e22b41c2fc31b734edd65dccc18acde5b71a
Jan Jurka <jurj16@vse.cz>
2025-03-06 19:20:51 +0100

    Add operator reset on Equal

--- Kalkulator.java
+++ Kalkulator.java
@@ -75,6 +75,7 @@ public class Kalkulator
             hodnotaZobrazeni = prvniOperant + hodnotaZobrazeni;
         else if (operator == '-')
             hodnotaZobrazeni = prvniOperant - hodnotaZobrazeni;
+        operator = ' ';
     }
     
     /**
