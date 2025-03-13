cbe8ae0298fc249a2dc9d2a7dfdb02932bcb26cf
Everesh <everesh@proton.me>
2025-03-13 19:06:40 +0100

    Add Ukoly

--- test/KalkulackaTest.java
+++ test/KalkulackaTest.java
@@ -39,4 +39,61 @@ public class KalkulackaTest {
         kalk.rovnaSe();
         assertEquals(37, kalk.getHodnotaKZobrazeni());
     }
+
+    @Test
+    public void testUkol1() {
+        kalk.cislice(3);
+        kalk.cislice(5);
+        kalk.plus();
+        assertEquals(35, kalk.getHodnotaKZobrazeni());
+        kalk.cislice(2);
+        assertEquals(2, kalk.getHodnotaKZobrazeni());
+        kalk.rovnaSe();
+        assertEquals(37, kalk.getHodnotaKZobrazeni());
+        kalk.cislice(2);
+        kalk.rovnaSe();
+        assertEquals(2, kalk.getHodnotaKZobrazeni());
+    }
+
+    @Test
+    public void testUkol2() {
+        kalk.cislice(3);
+        kalk.cislice(5);
+        kalk.plus();
+        assertEquals(35, kalk.getHodnotaKZobrazeni());
+        kalk.rovnaSe();
+        assertEquals(70, kalk.getHodnotaKZobrazeni());
+    }
+
+    @Test
+    public void testUkol3() {
+        kalk.cislice(3);
+        kalk.cislice(5);
+        kalk.plus();
+        assertEquals(35, kalk.getHodnotaKZobrazeni());
+        kalk.cislice(2);
+        kalk.rovnaSe();
+        assertEquals(37, kalk.getHodnotaKZobrazeni());
+        kalk.rovnaSe();
+        assertEquals(39, kalk.getHodnotaKZobrazeni());
+    }
+
+    @Test
+    public void testUkol4() {
+        kalk.cislice(2);
+        kalk.cislice(1);
+        kalk.cislice(4);
+        kalk.cislice(7);
+        kalk.cislice(4);
+        kalk.cislice(8);
+        kalk.cislice(3);
+        kalk.cislice(6);
+        kalk.cislice(4);
+        kalk.cislice(8);
+        assertEquals(214748364, kalk.getHodnotaKZobrazeni());
+        kalk.cislice(7);
+        assertEquals(2147483647, kalk.getHodnotaKZobrazeni());
+        kalk.cislice(8);
+        assertEquals(2147483647, kalk.getHodnotaKZobrazeni());
+    }
 }
